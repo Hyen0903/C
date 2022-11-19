2ef9554f087a97cf897e3d5929d38de379565bb4
신희재 <117503330+Hyen0903@users.noreply.github.com>
2022-11-19 15:26:09 +0900

    Create Nested loops.c

---
+++ Nested loops.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  for(int a = 1; a <= 6; a++) {
+    printf("\n %d학년 ", a);
+    for(int b = 1; b <= 7; b++) {
+      printf("%d반 ", b);
+    }
+  } 
+  //김 모씨 
+  return 0;
+}
